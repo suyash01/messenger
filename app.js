cefe3e8c77ac4a53de5afd05d7e456fce0fb0617
Suyash Mittal <suyash8514@gmail.com>
2017-11-24 01:57:47 +0530

    Updated to Bootstrap 4

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
-var express = require('express');
-var path = require('path');
-var app = express();
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
-var port = process.env.PORT || 3000;
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+
+const port = process.env.PORT || 3000;
 
 app.use(express.static(path.join(__dirname, '/public')));
 
